aba55ab8f79e9b8363216095249f85ccff97fb36
Thiago Leis <thiagossaleis139@gmail.com>
2025-08-21 16:39:33 -0300

    Cadastro de usuário com validação de segurança

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-# Garanta que AMBAS as views estão sendo importadas
-from .views import login_view, dashboard_view
+from .views import login_view, dashboard_view, register_view
 
 urlpatterns = [
-    # A URL de login que já tínhamos
     path('', login_view, name='login'),
-
-    # A URL do dashboard que estava faltando
     path('dashboard/', dashboard_view, name='dashboard'),
+    path('register/', register_view, name='register'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,9 @@
-import re 
+import re
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import RegistrationForm
 
 
 def login_view(request):
@@ -33,3 +34,18 @@ def login_view(request):
 @login_required
 def dashboard_view(request):
     return render(request, 'authentication/dashboard.html')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        # 1. Cria uma instância do formulário com os dados enviados (request.POST)
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            # Se for válido, salva o novo usuário no banco de dados.
+            # O form.save() já cuida de criptografar a senha!
+            form.save()
+            messages.success(request, 'Cadastro realizado com sucesso! Por favor, faça o login.')
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'authentication/register.html', {'form': form})
